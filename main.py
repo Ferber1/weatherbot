595e0f6b68ce8851bd1e9ef6a4b4b9d683c242ef
Ferber1 <nikita1991sidorenko@gmail.com>
2024-02-24 23:48:28 +0500

    Reading

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 #* uncomment import class of bot you need
-from bots.tgbot import TgBot
+# from bots.tgbot import TgBot
 # from bots.vkbot import VkBot
 
 if __name__ == '__main__':
@@ -16,5 +16,4 @@ if __name__ == '__main__':
     >>> bot = VkBot()
     >>> asyncio.run(bot.start())
     """
-    bot = TgBot()
-    asyncio.run(bot.start())
+    
